bdb167fd865719caf7ccd54478fdb51ec2aee250
Erik <erik.dubois@gmail.com>
2016-01-24 15:40:33 +0100

    Automatic Backup @ Sun Jan 24 15:40:33 CET 2016

--- installation/0_install_i3_mirors_v26.sh
+++ installation/0_install_i3_mirors_v26.sh
@@ -48,7 +48,7 @@
 
 
 # installing refector to test wich servers are fastest
-sudo pacman -S reflector
+sudo pacman -S reflector --noconfirm
 
 # finding the fastest archlinux servers
 sudo reflector -l 50 -f 20 --save /tmp/mirrorlist.new && rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist && sudo cp /tmp/mirrorlist /etc/pacman.d
