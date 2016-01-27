55382d49b0bf323ce4066023b344899e4aa203f0
erikdubois <erik.dubois@gmail.com>
2016-01-27 16:39:36 +0100

    Automatic Backup @ Wed 27 Jan 16:39:36 CET 2016

---
+++ installation/9_themes_icons.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#              `.-/::/-``
+#            .-/osssssssso/.               
+#           :osyysssssssyyys+-              
+#        `.+yyyysssssssssyyyyy+.           
+#       `/syyyyyssssssssssyyyyys-`         
+#      `/yhyyyyysss++ssosyyyyhhy/`         
+#     .ohhhyyyyso++/+oso+syy+shhhho.       
+#    .shhhhysoo++//+sss+++yyy+shhhhs.      
+#   -yhhhhs+++++++ossso+++yyys+ohhddy:     
+#  -yddhhyo+++++osyyss++++yyyyooyhdddy-    
+# .yddddhso++osyyyyys+++++yyhhsoshddddy`   
+#`odddddhyosyhyyyyyy++++++yhhhyosddddddo   
+#.dmdddddhhhhhhhyyyo+++++shhhhhohddddmmh.  
+#ddmmdddddhhhhhhhso++++++yhhhhhhdddddmmdy  
+#dmmmdddddddhhhyso++++++shhhhhddddddmmmmh  
+#-dmmmdddddddhhyso++++oshhhhdddddddmmmmd- 
+# .smmmmddddddddhhhhhhhhhdddddddddmmmms. 
+#   `+ydmmmdddddddddddddddddddmmmmdy/.     
+#      `.:+ooyyddddddddddddyyso+:.`
+#======================================================================================
+
+#======================================================================================
+#                               I3 IMPROVED TILING
+# 
+# Author  : Erik Dubois at http://www.erikdubois.be
+# License : Distributed under the terms of GNU GPL version 2 or later
+# 
+# AS ALLWAYS, KNOW WHAT YOU ARE DOING.
+#======================================================================================
+
+
+echo "################################################################"
+echo "themes"
+echo "################################################################"
+
+# dark gray theme
+packer blackbird --noconfirm --noedit
+
+
+
+echo "################################################################"
+echo "icons"
+echo "################################################################"
+
+
+echo "########################################"
+echo "######## T H E   E N D  + R E B O O T ##"
+echo "########################################"
