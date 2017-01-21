86aa31628ad4eb5a2ef68248982c12a35eb2c2b9
Erik Dubois <erik.dubois@gmail.com>
2017-01-21 02:12:46 +0100

    Comment :  on 02:12:46 21-01-2017

--- installation/100-install-core-software-v1.sh
+++ installation/100-install-core-software-v1.sh
@@ -12,7 +12,7 @@
 ##################################################################################################################
 
 #software from 'normal' repositories
-sudo pacman -S --noconfirm --needed arc-gtk-theme archey3 baobab bleachbit catfish clementine clipit curl
+sudo pacman -S --noconfirm --needed arc-gtk-theme archey3 baobab bleachbit catfish clementine curl
 sudo pacman -S --noconfirm --needed darktable dconf-editor
 sudo pacman -S --noconfirm --needed dmidecode 
 sudo pacman -S --noconfirm --needed evince evolution filezilla firefox
@@ -21,7 +21,7 @@ sudo pacman -S --noconfirm --needed gnome-font-viewer gnome-system-monitor gnome
 sudo pacman -S --noconfirm --needed gparted gpick grsync
 sudo pacman -S --noconfirm --needed hardinfo hddtemp hexchat htop 
 sudo pacman -S --noconfirm --needed inkscape inxi lm_sensors meld mlocate mpv
-sudo pacman -S --noconfirm --needed nemo numlockx openshot opera pinta plank 
+sudo pacman -S --noconfirm --needed nemo numlockx openshot opera pamac pinta plank 
 sudo pacman -S --noconfirm --needed redshift ristretto sane screenfetch scrot shotwell 
 sudo pacman -S --noconfirm --needed shutter simple-scan simplescreenrecorder smplayer sysstat 
 sudo pacman -S --noconfirm --needed terminator thunar transmission-cli transmission-gtk
