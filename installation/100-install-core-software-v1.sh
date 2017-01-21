4ec59337063b3a622534aabc93cb1cb4425373ca
Erik Dubois <erik.dubois@gmail.com>
2017-01-21 02:15:49 +0100

    Comment :  on 02:15:49 21-01-2017

--- installation/100-install-core-software-v1.sh
+++ installation/100-install-core-software-v1.sh
@@ -21,7 +21,7 @@ sudo pacman -S --noconfirm --needed gnome-font-viewer gnome-system-monitor gnome
 sudo pacman -S --noconfirm --needed gparted gpick grsync
 sudo pacman -S --noconfirm --needed hardinfo hddtemp hexchat htop 
 sudo pacman -S --noconfirm --needed inkscape inxi lm_sensors meld mlocate mpv
-sudo pacman -S --noconfirm --needed nemo numlockx openshot opera pamac pinta plank 
+sudo pacman -S --noconfirm --needed nemo numlockx openshot opera pamac pinta plank polkit-gnome 
 sudo pacman -S --noconfirm --needed redshift ristretto sane screenfetch scrot shotwell 
 sudo pacman -S --noconfirm --needed shutter simple-scan simplescreenrecorder smplayer sysstat 
 sudo pacman -S --noconfirm --needed terminator thunar transmission-cli transmission-gtk
