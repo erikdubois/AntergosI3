78e9de02ad5333da57dfc667602da8e33d3bb6b7
Erik Dubois <erik.dubois@gmail.com>
2017-01-25 21:42:39 +0100

    Comment :  on 21:42:39 25-01-2017

--- installation/040-install-packer-for-aur-make-build-v1.sh
+++ installation/040-install-packer-for-aur-make-build-v1.sh
@@ -24,19 +24,56 @@ set -e
 
 #dependencies for packer
 
-sudo pacman -S --noconfirm --needed grep sed bash curl pacman jshon expac
 
-rm -rf /tmp/packer
 
-mkdir /tmp/packer
+package="packer"
+command="packer"
 
-wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=packer
+#----------------------------------------------------------------------------------
 
-mv PKGBUILD\?h\=packer /tmp/packer/PKGBUILD
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
 
-cd /tmp/packer
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
 
-makepkg -i /tmp/packer --noconfirm
+else
+
+	sudo pacman -S --noconfirm --needed grep sed bash curl pacman jshon expac
+
+	[ -d /tmp/packer ] && rm -rf /tmp/packer
+
+	mkdir /tmp/packer
+
+	wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=packer
+
+	mv PKGBUILD\?h\=packer /tmp/packer/PKGBUILD
+
+	cd /tmp/packer
+
+	makepkg -i /tmp/packer --noconfirm
+
+	[ -d /tmp/packer ] && rm -rf /tmp/packer
+
+	# Just checking if installation was successful
+	if pacman -Qi $package &> /dev/null; then
+	
+	echo "################################################################"
+	echo "#########  "$package" has been installed"
+	echo "################################################################"
+
+	else
+
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!!!!!!!!  "$package" has NOT been installed"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+	fi
+
+fi
+
+
 
 
 #sudo pacman -U /tmp/packer/packer-*.pkg.tar.xz
