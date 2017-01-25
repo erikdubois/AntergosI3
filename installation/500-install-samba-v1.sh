a421163d9745a30e7fbcbd9f6239d2c54c94acd3
Erik Dubois <erik.dubois@gmail.com>
2017-01-25 22:29:59 +0100

    Comment :  on 22:29:59 25-01-2017

--- installation/500-install-samba-v1.sh
+++ installation/500-install-samba-v1.sh
@@ -18,6 +18,7 @@ sudo systemctl enable smbd.service
 sudo systemctl start smbd.service
 sudo systemctl enable nmbd.service
 sudo systemctl start nmbd.service
+##Change your username here
 sudo smbpasswd -a erik
 
 #access samba share windows
@@ -28,7 +29,64 @@ sudo pacman -S --noconfirm --needed gvfs-smb
 # testparm will check the conf file for errors
 
 # red hat samba sharing config 
-packer -S --noconfirm --noedit system-config-samba
+
+echo "################################################################"
+echo "system-config-samba"
+echo "################################################################"
+
+
+package="system-config-samba"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi packer &> /dev/null; then
+
+		echo "Installing with packer"
+		packer -S --noconfirm --noedit  $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+		
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+		 	
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+			  	
+	fi
+
+	# Just checking if installation was successful
+	if pacman -Qi $package &> /dev/null; then
+	
+	echo "################################################################"
+	echo "#########  "$package" has been installed"
+	echo "################################################################"
+
+	else
+
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!!!!!!!!  "$package" has NOT been installed"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+
+	fi
+
+fi
+
+
+
+
 
 echo "Run system-config-samba to set up shares"
 
