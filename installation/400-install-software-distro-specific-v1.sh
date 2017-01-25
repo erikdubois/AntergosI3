6fbd8e8c9fac5001288267a786f3ca53ef4dbeea
Erik Dubois <erik.dubois@gmail.com>
2017-01-25 20:16:25 +0100

    Comment :  on 20:16:25 25-01-2017

--- installation/400-install-software-distro-specific-v1.sh
+++ installation/400-install-software-distro-specific-v1.sh
@@ -44,19 +44,7 @@ sudo packer -S arandr  --noconfirm
 #run with systemadm
 sudo packer -S systemd-ui  --noconfirm 
 
-#sudo pacman -S gnome-terminal  --noconfirm --needed
-#Printer
 
-sudo pacman -S cups cups-pdf ghostscript gsfonts libcups hplip system-config-printer --noconfirm --needed
-systemctl enable org.cups.cupsd.service
-systemctl start org.cups.cupsd.service
-
-#Network
-
-sudo pacman -S networkmanager --noconfirm --needed
-sudo pacman -S network-manager-applet --noconfirm --needed
-sudo systemctl enable NetworkManager.service
-sudo systemctl start NetworkManager.service
 
 
 # if you want a networkmanager and applet mostly for laptop/wireless as
@@ -188,6 +176,22 @@ else
 fi
 
 
+#sudo pacman -S gnome-terminal  --noconfirm --needed
+#Printer
+
+sudo pacman -S cups cups-pdf ghostscript gsfonts libcups hplip system-config-printer --noconfirm --needed
+systemctl enable org.cups.cupsd.service
+systemctl start org.cups.cupsd.service
+
+#Network
+
+sudo pacman -S networkmanager --noconfirm --needed
+sudo pacman -S network-manager-applet --noconfirm --needed
+sudo systemctl enable NetworkManager.service
+sudo systemctl start NetworkManager.service
+
+
+
 ###############################################################
 #
 #        EXTRA      EXTRA      EXTRA      EXTRA     EXTRA      
@@ -195,23 +199,8 @@ fi
 ###############################################################
 
 
-        # D U A L  S C R E E N #
-
-# Dualscreen - this changes just type xrandr and change the numbers
-# xrandr --output DVI-I-0 --auto --left-of DVI-I-3
-
-        # K E Y B O A R D #  
-
-# if keyboard not azerty put in config file
-# exec setxkbmap be 
-
-        # X P R O P #
-
-# with xprop you can find out what the class name is of a program
-# to set it floating for example
-
         # A U T O  L O G I N #
-
+# alternative way to autologin that worked a year ago
 # if you want to autologin - aka at home no danger
 # https://github.com/Arakis/autologin-on-boot
 # packer autologin-on boot --noedit
@@ -219,59 +208,6 @@ fi
 # sudo systemctl enable autologin-on-boot
 # sudo systemctl start autologin-on-boot
 
-
-        # S P E E D T E S T #
-
-# terminal speedtest
-# wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest_cli.py
-# chmod +x speedtest-cli
-# ./speedtest-cli --share
-# https://github.com/sivel/speedtest-cli
-
-        
-
-        # B U R N   I S O  T O   U S B #
-
-# sudo fdisk -l
-# sudo dd bs=4M if=/path/to/xxxxx.iso of=/dev/sdc && sync
-
-
-        # U P D A T E #
-
-# update notifier
-
-# i3blocks has a little script that says how many updates there are
-# just run at your convenience sudo pacman -Syu
-
-# I would choose pamac as notifier
-# IF you want an update manager to tell you there are updates (icontray)
-# pamac is the most complete one
-# download from https://github.com/manjaro/pamac or
-# git clone https://github.com/manjaro/pamac.git
-# go to download folder
-# Type : make
-# Type : make install
-# use pamac-tray in combination with notify-osd
-# put pamac-tray in config of i3
-# updates for repo
-# sudo pacman -Syu
-# updates for aur
-# packer -Syu --noedit
-
-
-
-##############################################
-##########   A   P   P   L   E    ############
-##########     MacBookPro 5.4     ############
-##############################################
-#
-# macbookpro fan control for manjaro 0.8.12 Ascella
-# packer macfanctld-git --noedit
-# WHEN kernel update possibly missing not sure if I need it
-# packer aic94xx --noedit
-#
-##############################################
-###############################################################################################
 
 echo "################################################################"
 echo "#########   distro specific software installed  ################"
