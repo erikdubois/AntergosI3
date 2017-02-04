d21da066a8c7e7c9e7fca41860199b21137e2ffe
Erik Dubois <erik.dubois@gmail.com>
2017-02-04 08:41:35 +0100

    Comment :  on Sat Feb  4 08:41:35 CET 2017

--- setup-git.sh
+++ setup-git.sh
@@ -5,16 +5,37 @@
 # Author 	: 	Erik Dubois
 # Website 	: 	http://www.erikdubois.be
 ##################################################################################################################
-# 
-
 ##################################################################################################################
 #
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
 
+# Standard text output from Github
 
-#checking if git is installed else install it
+# echo "# Github Tutorial" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git remote add origin https://github.com/erikdubois/Github-Tutorial.git
+# git push -u origin master
+
+
+# Problem solving commands
+
+# Read before using it.
+# https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
+# git reset --hard orgin/master
+# ONLY if you are very sure and no coworkers are on your github.
+
+# Command that have helped in the past
+# Force git to overwrite local files on pull - no merge
+# git fetch all
+# git push --set-upstream origin master
+# git reset --hard orgin/master
+
+
+# installing git if not installed for specific distro's
 
 if ! location="$(type -p "git")" || [ -z "git" ]; then
 
@@ -23,6 +44,27 @@ if ! location="$(type -p "git")" || [ -z "git" ]; then
 	echo "#################################################"
 
   	sudo apt install git -y
+	# check if apt-git is installed
+	if which apt-get > /dev/null; then
+
+		sudo apt-get install -y git
+
+	fi
+
+	# check if pacman is installed
+	if which pacman > /dev/null; then
+
+		sudo pacman -S --noconfirm git
+
+	fi
+
+	# check if eopkg is installed
+	if which eopkg > /dev/null; then
+
+		sudo eopkg -y install git
+
+	fi
+
 fi
 
 
@@ -36,11 +78,14 @@ git config --global user.name "Erik Dubois"
 git config --global user.email "erik.dubois@gmail.com"
 sudo git config --system core.editor nano
 git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=3600'
+git config --global credential.helper 'cache --timeout=18000'
 git config --global push.default simple
 
 
 
+#git remote add origin https://github.com/erikdubois/Ultimate-Linux-Mint-18-Cinnamon.git
 
 
-echo " ALL  D O N E !"
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
